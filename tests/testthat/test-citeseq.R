635530334f5f9cca27b95180fa58b56cdd95df6f
bbimber <bbimber@gmail.com>
2021-02-04 17:31:29 -0800

    Citeseq (#26)
    * Remove cite-seq, which is now in CellMembrane
    * Remove manual install of DelayedMatrixStats in Docker, now that their install bug is fixed

--- tests/testthat/test-citeseq.R
+++
@@ -1,9 +0,0 @@
-context("citeseq")
-
-test_that("CITE-seq plots work", {
-
-	LoadCiteSeqCountData(rawCountData = '../testdata/458-6-GEX.citeSeqCounts.2.citeseq/umi_count')
-
-	PlotLibrarySaturationByMarker('../testdata/458-6-GEX.citeSeqCounts.2.citeseq/')
-
-})
