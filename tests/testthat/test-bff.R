0a96c2f39e5b68ebd32764bcc6cce2445feba75d
bbimber <bbimber@gmail.com>
2021-05-05 20:46:58 -0700

    Update test

--- tests/testthat/test-bff.R
+++ tests/testthat/test-bff.R
@@ -3,6 +3,7 @@ context("scRNAseq")
 source('testing-data.R')
 
 test_that("BF Works", {
-	data <- read.csv('../testdata/MS/cell_type_counts.csv')
+	barcodeData <- t(as.matrix(read.csv('../testdata/MS/cell_type_counts.csv', row.names = 1)))
 	
+	df <- GenerateCellHashingCalls(barcodeMatrix = barcodeData, methods = c('bff_quantile'))
 })
