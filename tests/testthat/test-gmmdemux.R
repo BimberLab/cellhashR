12337c472ee60f0ae2a36183e1bb74d93e654ba2
bbimber <bbimber@gmail.com>
2025-02-22 17:49:36 -0800

    Test update

--- tests/testthat/test-gmmdemux.R
+++ tests/testthat/test-gmmdemux.R
@@ -8,20 +8,20 @@ test_that("GMM-Demux Works", {
 	print(table(df$consensuscall))
 	
 	expectedCalls <- list(
-		Bar1 = 192,
+		Bar1 = 191,
 		Bar2 = 143,
-		Bar3 = 377,
+		Bar3 = 373,
 		Bar4 = 467,
-		Bar5 = 144,
-		Bar6 = 493,
+		Bar5 = 148,
+		Bar6 = 487,
 		Bar7 = 564,
-		Bar8 = 528,
-		Bar9 = 448,
-		Bar10 = 498,
-		Bar11 = 482,
-		Bar12 = 637,
-		Doublet = 566,
-		Negative = 257
+		Bar8 = 524,
+		Bar9 = 447,
+		Bar10 = 496,
+		Bar11 = 478,
+		Bar12 = 634,
+		Doublet = 588,
+		Negative = 255
 	)
 
 	for (hto in unique(df$consensuscall)) {
